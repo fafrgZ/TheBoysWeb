9d7cf582e1defafaa74f2c88b083421ce26b4146
fafrgZ <111191607+fafrgZ@users.noreply.github.com>
2024-07-05 00:06:55 -0300

    agregando el contacto

--- Js/scripts.js
+++ Js/scripts.js
@@ -100,3 +100,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }   
 });
+
+document.querySelector('.nav-toggle').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('active');
+});
